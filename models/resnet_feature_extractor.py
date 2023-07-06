77aeb6b208081048d61deef31951ea03963ebec2
faisal-kt <muhammad.faisal@gomotive.com>
2023-07-06 13:21:00 +0500

    resnet backbone

--- models/resnet_feature_extractor.py
+++ models/resnet_feature_extractor.py
@@ -24,13 +24,26 @@ class ResNetFeatureExtractor(nn.Module):
 
         model = torchvision.models.__dict__[backbone_name](weights='IMAGENET1K_V1' if pretrained else None)
 
-        self.features = create_feature_extractor(model, return_nodes=return_nodes)
+        all_layers = list(model.children())[:-2]
+        #
+        self.features1 = nn.Sequential(*all_layers[:4])
+        self.features2 = all_layers[4]
+        self.features3 = all_layers[5]
+        self.features4 = all_layers[6]
+        self.features5 = all_layers[7]
+
+        # self.features = create_feature_extractor(model, return_nodes=return_nodes)
         self.channel = [i.size(1) for i in self.forward(torch.randn(2, 3, 640, 640))]
+        # self.channel = [64, 256, 512, 1024, 2048]
 
 
     def forward(self, x):
-        features = self.features(x)
-        return [v for k, v in features.items()]
+        x1 =  self.features1(x)
+        x2 =  self.features2(x1)
+        x3 =  self.features3(x2)
+        x4 =  self.features4(x3)
+        x5 =  self.features5(x4)
+        return [x1, x2, x3, x4, x5]
 
 
 if __name__ == "__main__":
